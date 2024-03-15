a5a7cfb52e78eef3196129c452569ab535d2db2d
jeison poveda <jeisonfpovedac@gmail.com>
2024-03-15 00:21:46 -0500

    ADD - Footer adding mobile version

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,14 +2,25 @@ import { Component, input } from '@angular/core';
 import { HamburgerComponent } from '../atoms/hamburger/hamburger.component';
 import { LogoComponent } from '../atoms/logo/logo.component';
 import { LanguageComponent } from '../atoms/language/language.component';
-
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [HamburgerComponent, LogoComponent, LanguageComponent],
+  imports: [
+    HamburgerComponent,
+    LogoComponent,
+    LanguageComponent,
+    SidebarComponent,
+    CommonModule
+  ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
 
+  status: boolean = false;
+  clickEvent() {
+    this.status = !this.status;
+  }
 }
