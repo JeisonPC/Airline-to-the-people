f1be2051c8f8ebe2e1f266f8dfc76bd48f8d31c5
jeison poveda <jeisonfpovedac@gmail.com>
2024-03-14 21:02:06 -0500

    FIX - Se corrigió altura del header

--- src/app/components/atoms/language/language.component.ts
+++ src/app/components/atoms/language/language.component.ts
@@ -6,12 +6,5 @@ import { Component } from '@angular/core';
   templateUrl: './language.component.svg',
 })
 export class LanguageComponent {
-  fillColor = 'rgb(255, 0, 0)';
 
-  changeColor() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    this.fillColor = `rgb(${r}, ${g}, ${b})`;
-  }
 }
